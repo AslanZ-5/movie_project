8e5c25ef9b024110f1607f212d87830e73bc5bec
Aslan <asl.zurabov@gmail.com>
2021-10-29 17:21:33 +0300

    category and actor model created

--- main/models.py
+++ main/models.py
@@ -1,3 +1,29 @@
 from django.db import models
 
-# Create your models here.
+class CategoryModel(models.Model):
+    name = models.CharField(max_length=255)
+    description = models.TextField()
+    url = models.SlugField(max_length=120)
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = 'Category'
+        verbose_name_plural = 'Categories'
+
+
+class Actor(models.Model):
+    name = models.CharField(max_length=255)
+    age = models.SmallIntegerField(default=0)
+    description = models.TextField()
+    image = models.ImageField(upload_to='actors/')
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = 'Actor and director'
+        verbose_name_plural = 'Actors and directors'
+
+
