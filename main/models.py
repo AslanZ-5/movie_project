fddc49176bb979f0237572e56bb2f708cd4116e0
Aslan <asl.zurabov@gmail.com>
2021-11-01 15:35:50 +0300

    get_absolute_url for detail

--- main/models.py
+++ main/models.py
@@ -1,6 +1,7 @@
 import datetime
 
 from django.db import models
+from django.urls import reverse
 
 
 class CategoryModel(models.Model):
@@ -63,6 +64,9 @@ class Movie(models.Model):
 
     def __str__(self):
         return self.title
+
+    def get_absolute_url(self):
+        return reverse('movie_detail', kwargs={'slug': self.url})
 
     class Meta:
         verbose_name = 'Movie'
