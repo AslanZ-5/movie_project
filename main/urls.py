dd894acdaafa3c1a7bd04f6eb2f7366a5c5986f7
Aslan <asl.zurabov@gmail.com>
2021-11-01 16:52:14 +0300

    creating review

--- main/urls.py
+++ main/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path,include
-from .views import MoviesView,MoviesDetailView
+from .views import MoviesView,MoviesDetailView,AddReview
 
 urlpatterns = [
     path('',MoviesView.as_view(),name='movies'),
-    path('detail/<str:slug>/',MoviesDetailView.as_view(),name='movie_detail')
+    path('detail/<str:slug>/',MoviesDetailView.as_view(),name='movie_detail'),
+    path('<int:id>/',AddReview.as_view(),name='add_review')
 ]
--- main/views.py
+++ main/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render
-from .models import Movie
+from django.shortcuts import render, redirect
+
+from .models import Movie,Reviews
 from django.views.generic import (
     ListView,
     DetailView,
@@ -24,7 +25,18 @@ class MoviesDetailView(DetailView):
     model = Movie
     slug_field = 'url'
 
+
 # class MoviesDetailView(View):
 #     def get(self, request, slug):
 #         movie = Movie.objects.get(url=slug)
 #         return render(request, 'main/movie_detail.html', {'movie': movie})
+
+
+class AddReview(View):
+    def post(self, request, id):
+        query = request.POST
+        print(id)
+        print(query)
+        review = Reviews.objects.create(name=query['name'],email=query['email'],text=query['text'],movie_id=id)
+        review.save()
+        return redirect('/')
