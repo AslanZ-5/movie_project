d484b32c7a9bd5ed7e30f2adf7e77fe9e09dd1b1
Aslan <asl.zurabov@gmail.com>
2021-11-02 16:58:21 +0300

    review admin and movie inline

--- main/admin.py
+++ main/admin.py
@@ -7,16 +7,26 @@ class CategoryAdmin(admin.ModelAdmin):
     list_display = ['id', 'name', 'url']
     list_display_links = ['id', 'name']
 
+class ReviewLine(admin.StackedInline):
+    model = Reviews
+    extra = 1
+
 
 @admin.register(Movie)
 class MovieAdmin(admin.ModelAdmin):
     list_display = ['title', 'category', 'url', 'draft']
-    list_filter = ['category','year']
-    search_fields = ('title','category__name')
+    list_filter = ['category', 'year']
+    search_fields = ('title', 'category__name')
+    inlines = [ReviewLine]
+
+@admin.register(Reviews)
+class ReviewAdmin(admin.ModelAdmin):
+    list_display = ['name', 'email', 'parent', 'movie']
+    readonly_fields = ['name','email']
+
 
 admin.site.register(MovieShots)
 admin.site.register(Actor)
 admin.site.register(Genre)
 admin.site.register(Rating)
 admin.site.register(RatingStart)
-admin.site.register(Reviews)
