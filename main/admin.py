a977bb5a3375fc7c97069ea1586019ebd396ff68
Aslan <asl.zurabov@gmail.com>
2021-11-02 18:28:07 +0300

    rendering actor image in admin

--- main/admin.py
+++ main/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
 from .models import *
+from django.utils.safestring import mark_safe
 
 
 @admin.register(CategoryModel)
@@ -28,21 +29,21 @@ class MovieAdmin(admin.ModelAdmin):
             'fields': (('title', 'tagline'),)
         }),
         (None, {
-            'fields': ('description','poster')
+            'fields': ('description', 'poster')
         }),
         (None, {
-            'fields': (('year', 'country','world_premiere'),)
+            'fields': (('year', 'country', 'world_premiere'),)
         }),
 
         (None, {
-            'fields': (('actors', 'directors','genres','category'),)
+            'fields': (('actors', 'directors', 'genres', 'category'),)
         }),
         (None, {
             'fields': (('budget', 'fees_in_usa', 'fees_in_world'),)
         }),
         ('url and draft', {
-            'classes':('collapse',),
-            'fields': (('url','draft'),)
+            'classes': ('collapse',),
+            'fields': (('url', 'draft'),)
         })
 
     )
@@ -54,10 +55,31 @@ class ReviewAdmin(admin.ModelAdmin):
     readonly_fields = ['name', 'email']
 
 
+@admin.register(Genre)
+class GenreAdmin(admin.ModelAdmin):
+    list_display = ['name', 'url']
 
 
-admin.site.register(MovieShots)
-admin.site.register(Actor)
-admin.site.register(Genre)
-admin.site.register(Rating)
-admin.site.register(RatingStart)
+@admin.register(Actor)
+class ActorAdmin(admin.ModelAdmin):
+    list_display = ['name', 'age','get_image']
+    readonly_fields = ('get_image',)
+
+    def get_image(self,obj):
+        return mark_safe(f'<img src="{obj.image.url}" style="width:40px;height:40px;"')
+    get_image.short_description = 'Image'
+
+
+@admin.register(MovieShots)
+class MovieShotsAdmin(admin.ModelAdmin):
+    list_display = ['title','movie' ]
+
+
+@admin.register(Rating)
+class RatingAdmin(admin.ModelAdmin):
+    list_display = ['ip', 'star', 'movie']
+
+
+@admin.register(RatingStart)
+class RatingStartAdmin(admin.ModelAdmin):
+    list_display = ['value' ]
