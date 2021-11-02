d2322685d4bc1b59717f152eaaa5d9c49554d50e
Aslan <asl.zurabov@gmail.com>
2021-11-02 16:50:52 +0300

    movie class admin customization

--- main/admin.py
+++ main/admin.py
@@ -8,7 +8,12 @@ class CategoryAdmin(admin.ModelAdmin):
     list_display_links = ['id', 'name']
 
 
-admin.site.register(Movie)
+@admin.register(Movie)
+class MovieAdmin(admin.ModelAdmin):
+    list_display = ['title', 'category', 'url', 'draft']
+    list_filter = ['category','year']
+    search_fields = ('title','category__name')
+
 admin.site.register(MovieShots)
 admin.site.register(Actor)
 admin.site.register(Genre)
