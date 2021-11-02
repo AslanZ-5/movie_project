18550e5a953416c3845e87784b9597c485382603
Aslan <asl.zurabov@gmail.com>
2021-11-02 18:03:09 +0300

    admin customizing

--- main/admin.py
+++ main/admin.py
@@ -7,9 +7,11 @@ class CategoryAdmin(admin.ModelAdmin):
     list_display = ['id', 'name', 'url']
     list_display_links = ['id', 'name']
 
-class ReviewLine(admin.StackedInline):
+
+class ReviewLine(admin.TabularInline):
     model = Reviews
     extra = 1
+    readonly_fields = ['name']
 
 
 @admin.register(Movie)
@@ -18,11 +20,40 @@ class MovieAdmin(admin.ModelAdmin):
     list_filter = ['category', 'year']
     search_fields = ('title', 'category__name')
     inlines = [ReviewLine]
+    save_on_top = True
+    save_as = True
+    list_editable = ('draft',)
+    fieldsets = (
+        (None, {
+            'fields': (('title', 'tagline'),)
+        }),
+        (None, {
+            'fields': ('description','poster')
+        }),
+        (None, {
+            'fields': (('year', 'country','world_premiere'),)
+        }),
+
+        (None, {
+            'fields': (('actors', 'directors','genres','category'),)
+        }),
+        (None, {
+            'fields': (('budget', 'fees_in_usa', 'fees_in_world'),)
+        }),
+        ('url and draft', {
+            'classes':('collapse',),
+            'fields': (('url','draft'),)
+        })
+
+    )
+
 
 @admin.register(Reviews)
 class ReviewAdmin(admin.ModelAdmin):
     list_display = ['name', 'email', 'parent', 'movie']
-    readonly_fields = ['name','email']
+    readonly_fields = ['name', 'email']
+
+
 
 
 admin.site.register(MovieShots)
