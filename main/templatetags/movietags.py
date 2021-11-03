d69eb6eb0368de1fd5b6ff6da4b739a90ae29e94
Aslan <asl.zurabov@gmail.com>
2021-11-03 15:53:31 +0300

    templatetag for lastadded movies rendering with inclusivetag

--- main/templatetags/movietags.py
+++ main/templatetags/movietags.py
@@ -1,8 +1,15 @@
 from django import template
-from main.models import CategoryModel
+from main.models import CategoryModel, Movie
 
 register = template.Library()
+
 
 @register.simple_tag()
 def get_categ():
     return CategoryModel.objects.all()
+
+
+@register.inclusion_tag('main/last_added.html')
+def get_last_movie(count = 5):
+    movies = Movie.objects.order_by('id')[:count]
+    return {'last_movies': movies}
