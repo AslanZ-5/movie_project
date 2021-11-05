227da4883f37c410b1f25f8779290bf63d9918a4
Aslan <asl.zurabov@gmail.com>
2021-11-05 15:59:18 +0300

    searching films by their names

--- main/views.py
+++ main/views.py
@@ -1,12 +1,12 @@
 from django.shortcuts import render, redirect
 from .forms import ReviewForm, RatingForm
-from .models import Movie, Reviews, CategoryModel, Actor, Genre,Rating,RatingStart
+from .models import Movie, Reviews, CategoryModel, Actor, Genre, Rating, RatingStart
 from django.views.generic import (
     ListView,
     DetailView,
 )
 from django.db.models import Q
-from django.http import HttpResponse,HttpResponseRedirect
+from django.http import HttpResponse, HttpResponseRedirect
 
 from django.views.generic.base import View
 
@@ -100,7 +100,7 @@ class FilterMoviesView(GenreYear, ListView):
         return queryset
 
     def get_context_data(self, *args, **kwargs):
-        context = super().get_context_data(*args,**kwargs)
+        context = super().get_context_data(*args, **kwargs)
         context['year'] = ''.join([f'year={x}&' for x in self.request.GET.getlist('year')])
         context['genre'] = ''.join([f'genre={x}&' for x in self.request.GET.getlist('genre')])
         return context
@@ -119,10 +119,21 @@ class AddRatingView(View):
         form = RatingForm(request.POST)
         if form.is_valid():
             Rating.objects.update_or_create(
-                ip= self.get_client_ip(request),
+                ip=self.get_client_ip(request),
                 movie_id=int(request.POST.get("movie")),
-                defaults = {'star_id': int(request.POST.get('star'))}
+                defaults={'star_id': int(request.POST.get('star'))}
             )
             return HttpResponseRedirect('/')
         else:
-            return HttpResponse(status=400 )
+            return HttpResponse(status=400)
+
+
+class SearchMovie(ListView):
+    template_name = 'main/movies.html'
+    paginate_by = 2
+
+    def get_queryset(self):
+        queryset = Movie.objects.filter(title__icontains=self.request.GET.get('q'))
+        return queryset
+
+    
