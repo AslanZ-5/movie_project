81d790621c8d1e03dbb3c182c8524b4841f47ac1
Aslan <asl.zurabov@gmail.com>
2021-11-07 12:52:14 +0300

    adding multilanguages in site (customaizing settings and urls)  step 1

--- djmovie/settings.py
+++ djmovie/settings.py
@@ -49,6 +49,7 @@ INSTALLED_APPS = [
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.locale.LocaleMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
@@ -117,6 +118,13 @@ USE_I18N = True
 USE_L10N = True
 
 USE_TZ = True
+
+
+gettext = lambda s: s
+LANGUAGES = (
+    ('ru', gettext('Russia')),
+    ('en', gettext('English')),
+)
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
--- djmovie/urls.py
+++ djmovie/urls.py
@@ -14,18 +14,21 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path,include
+from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
-
+from django.conf.urls.i18n import i18n_patterns
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('ckeditor/', include('ckeditor_uploader.urls')),
-    path('pages/', include('django.contrib.flatpages.urls')),
-    path('',include('main.urls')),
-    path('contact/',include('contact.urls')),
-]
+    path('i18n/', include('django.conf.urls.i18n')),
 
+]
+urlpatterns += i18n_patterns(
+    path('pages/', include('django.contrib.flatpages.urls')),
+    path('', include('main.urls')),
+    path('contact/', include('contact.urls')),
+)
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
     urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
